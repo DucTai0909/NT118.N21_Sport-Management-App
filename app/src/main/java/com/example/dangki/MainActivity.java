27cc154d03199d9cd9d99fd41937b038fd33c43b
Duc Tai <nguyentoductai@gmail.com>
2023-05-10 11:55:01 +0700

    sua giao dien dang ky

---
+++ app/src/main/java/com/example/dangki/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.dangki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //hiding the title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/main/java/com/example/dangki/Register.java
@@ -0,0 +1,14 @@
+package com.example.dangki;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity {
+    EditText userName, password, fullName, phoneNumber;
+    Button BtnGegister;
+    FirebaseAuth fAuth;
+}
