a48255279efba1923ebffe01b005d93bbb9de666
Duc Tai <nguyentoductai@gmail.com>
2023-05-16 22:19:16 +0700

    Welconme screen

--- app/src/main/java/com/example/dangki/MainActivity.java
+++ app/src/main/java/com/example/dangki/MainActivity.java
@@ -5,18 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView currentTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_login);
-        //hiding the title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
-           Intent intent = new Intent(this, Register.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_welcome);
+
+        currentTime = findViewById(R.id.textViewCurrentTime);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
+        String currentTimeString = dateFormat.format(new Date());
+        currentTime.setText(currentTimeString);
+
+        //hiding the title bar
+
+//           Intent intent = new Intent(this, Register.class);
+//        startActivity(intent);
+//        finish();
     }
 }
